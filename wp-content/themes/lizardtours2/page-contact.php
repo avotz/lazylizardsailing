110a79134695183781948fa8597bfc04b6ec13a4
Alonso Avotz <alonso@avotz.com>
2017-04-03 16:39:13 -0600

    se cambio mapa de lazy lizar en contact

--- wp-content/themes/lizardtours2/page-contact.php
+++ wp-content/themes/lizardtours2/page-contact.php
@@ -10,7 +10,7 @@ get_header(); ?>
                 <div class="inner no-padding">
                 <?php if(function_exists('rdfa_breadcrumb')){ rdfa_breadcrumb(); } ?>
                     <div class="contact__map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1649.718486795047!2d-85.77333080701916!3d10.444260510196033!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x723857155e6c02f!2sLizard+Tours!5e0!3m2!1ses!2scr!4v1472930722748" width="600" height="480" frameborder="0" style="border: 0px; allowfullscreen=""></iframe>
+                       <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9332.274875733017!2d-85.77323439176044!3d10.442333752910777!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xead36e9e385a89e5!2sLazy+Lizard+Sailing!5e0!3m2!1ses!2s!4v1475527576038" width="1000" height="400" frameborder="0" style="border: 0px; pointer-events: none;" allowfullscreen=""></iframe>
                     </div>
                     <div class="contact__form">
                         
