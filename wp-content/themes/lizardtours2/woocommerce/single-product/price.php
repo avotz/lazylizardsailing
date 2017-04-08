164b6e2103957ed4a96fa1a0c7f4f0f547de59e7
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-04-08 09:44:12 -0600

    se quito from del precio

--- wp-content/themes/lizardtours2/woocommerce/single-product/price.php
+++ wp-content/themes/lizardtours2/woocommerce/single-product/price.php
@@ -21,15 +21,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $product;
-
+$custom_price_label = get_post_meta( $product->id, 'custom_price_label', true );
+$custom_price_individual = get_post_meta( $product->id, 'custom_price_individual', true );
 ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
 
-	<p class="price"><?php echo $product->get_price_html(); ?></p>
-
+	<p class="price"><?php echo $product->get_price_html(); ?>
+	<?php if($custom_price_label != '') :?>
+		<br><small style="color: #000;"><?php echo $custom_price_label ?> - <?php echo $custom_price_individual ?></small> 
+	<?php endif ?>
+	</p>
 	<meta itemprop="price" content="<?php echo esc_attr( $product->get_display_price() ); ?>" />
 	<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
 	<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
-	
+
 </div>
-<!-- <a href="#request-popup" class="request-popup-link btn-reserve"> Reserve Now</a> -->
